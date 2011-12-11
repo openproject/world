9b0030775677b91015a98c9b3d6e63dd62026563
jayfeng <jayfeng@jayfeng-ubuntu.(none)>
2011-12-11 18:04:23 +0800

    add the base healthworld setting activity

--- healthworld/src/com/tianxia/app/healthworld/AppApplication.java
+++ healthworld/src/com/tianxia/app/healthworld/AppApplication.java
@@ -1,11 +1,16 @@
 package com.tianxia.app.healthworld;
 
+import com.tianxia.app.healthworld.setting.SettingTabActivity;
 import com.tianxia.lib.baseworld.BaseApplication;
 
 public class AppApplication extends BaseApplication {
 
     @Override
     public void fillTabs() {
-        
+        tabActivitys.add(SettingTabActivity.class);
+
+        tabNormalImages.add(R.drawable.setting_normal);
+
+        tabPressImages.add(R.drawable.setting_press);
     }
 }
