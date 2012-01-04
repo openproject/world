8664a4642813bceac1fdf616a9d45e24560a9717
jayfeng <jayfeng@jayfeng-ubuntu.(none)>
2012-01-04 22:53:36 +0800

    start dev the base sqliteohelper class

--- baseworld/src/com/tianxia/lib/baseworld/BaseApplication.java
+++ baseworld/src/com/tianxia/lib/baseworld/BaseApplication.java
@@ -3,29 +3,35 @@ package com.tianxia.lib.baseworld;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.tianxia.lib.baseworld.db.BaseSQLiteHelper;
+
 import android.app.Application;
 
 public abstract class BaseApplication extends Application {
 
-    protected List<Class<?>> tabActivitys = new ArrayList<Class<?>>();
-    protected static List<Integer> tabNormalImages = new ArrayList<Integer>();
-    protected static List<Integer> tabPressImages = new ArrayList<Integer>();
-    
+    public BaseSQLiteHelper mSQLiteHelper;
+
+    protected List<Class<?>> mTabActivitys = new ArrayList<Class<?>>();
+    protected static List<Integer> mTabNormalImages = new ArrayList<Integer>();
+    protected static List<Integer> mTabPressImages = new ArrayList<Integer>();
+
     @Override
     public void onCreate() {
         fillTabs();
+
+        initDb();
     }
 
     public List<Class<?>> getTabActivitys(){
-        return tabActivitys;
+        return mTabActivitys;
     }
 
     public List<Integer> getTabNormalImages(){
-        return tabNormalImages;
+        return mTabNormalImages;
     }
     
     public List<Integer> getTabPressImages(){
-        return tabPressImages;
+        return mTabPressImages;
     }
 
     /**
@@ -35,5 +41,6 @@ public abstract class BaseApplication extends Application {
      * <li>tab press background resId</li>
      */
     public abstract void fillTabs();
-    
+
+    public abstract void initDb();
 }
---
+++ baseworld/src/com/tianxia/lib/baseworld/db/BaseSQLiteHelper.java
@@ -0,0 +1,37 @@
+package com.tianxia.lib.baseworld.db;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteDatabase.CursorFactory;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class BaseSQLiteHelper extends SQLiteOpenHelper {
+
+    public String mCreateSql;
+    public String mUpgradeSql;
+
+    public BaseSQLiteHelper(Context context, String name, CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    public void InitCreateSql() {
+        
+    }
+    public void InitUpgradeSql() {
+        
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        InitCreateSql();
+        db.execSQL(mCreateSql);
+        System.out.println("Base onCreate");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        InitUpgradeSql();
+
+    }
+
+}
