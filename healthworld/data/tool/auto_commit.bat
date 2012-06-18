8363878fbe9cf5f3b84d30453baf04bb111c7fcd
jayfeng <673592063@qq.com>
2012-06-18 23:15:14 +0800

    upload the latest python script and data getted from sina server

---
+++ healthworld/data/tool/auto_commit.bat
@@ -0,0 +1,5 @@
+python latestgen
+python pagegen
+cd ..
+svn add * --force
+svn commit -m "auto commit at %date% %time%"
---
+++ healthworld/data/tool/auto_plan.py
@@ -0,0 +1,6 @@
+﻿import time  
+import os
+
+while(True):
+    os.system("auto_commit.bat")
+    time.sleep(50)
