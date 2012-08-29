510e49d93e4c97acf65d855c57eb2e2775521c98
openproject <673592063@qq.com>
2012-08-29 13:30:21 +0800

    add some markets and remove unused SEND_SMS permission

--- healthworld/build.sh
+++ healthworld/build.sh
@@ -2,10 +2,11 @@
 basedir=$(cd "$(dirname "$0")";pwd)
 cd $basedir
 
-markets="google gfan official dev"
+#markets="google appchina waps gfan 91 hiapk goapk mumayi eoe nduo feiliu crossmo huawei qq 3g 360 baidu sohu 163 samsung coolmart meizu moto xiaomi lenovo nearme official dev"
+markets="gfan dev"
 for market in $markets
 do
     echo packaging healthworld_1.0_$market.apk ...
     sed -i "s/\(android:value=\)\"\(.*\)\"\( android:name=\"UMENG_CHANNEL\"\)/\1\"$market\"\3/g" AndroidManifest.xml
-    ant -Dapk-name=healthworld -Dapk-version=v1.0_alpha -Dapk-market=$market
+    ant -Dapk-name=healthworld -Dapk-version=v1.0_beta -Dapk-market=$market
 done
